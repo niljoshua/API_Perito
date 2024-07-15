28a54629ea14c59e86cf66c2d16ebe8e80c7c6a1
Nil Joshua <niljoshuaa@gmail.com>
2024-07-15 14:44:54 -0300

    atualizacao

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 const cors = require("cors");
 const port = process.env.PORT || 3001; 
-const routes = require('./routes.json');
     
 const app = jsonServer.create();
 const router = jsonServer.router("dados.json");
@@ -10,11 +9,7 @@ const dbcotas = jsonServer.router("cotas.json");
     
 app.db = router.db, dbcotas.db;
     
-const rules = auth.rewriter(
-routes);
-    
 app.use(cors());
-app.use(rules);
 app.use(auth);
 app.use(router);
 app.use(dbcotas);
